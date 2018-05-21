b89669e146d8e509536a9253094914d0179f0513
Samy Abou-Shama <sa@nomad.de>
2018-05-22 00:21:20 +0200

    Update config to include std plugin
    and added new functions to app.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,84 @@
-const {Button, TextView, ui} = require('tabris');
+const { Button, TextView, ui } = require('tabris');
 
+
+global.atob = require('atob');
+
+let battery = {
+  service: '180A',
+  level: '2A35'
+};
+
+let bloodpressure = {
+  service: '1810',
+  level: '2A49'
+};
+
+let deviceName = {
+  service: '1800',
+  level: '2A00'
+};
 
 let foundDevices = [];
 
 
 let button = new Button({
   centerX: 0, top: 100,
-  text: 'Show message'
+  text: 'Scan'
+}).appendTo(ui.contentView);
+
+let connectBtn = new Button({
+  centerX: 0, top: [button, 15],
+  text: 'Connect'
 }).appendTo(ui.contentView);
 
 let textView = new TextView({
-  centerX: 0, top: [button, 50],
+  centerX: 0, top: [connectBtn, 50],
   font: '24px'
 }).appendTo(ui.contentView);
 
+connectBtn.on('select', () => {
+  global.ble.connect('18:7A:93:12:0E:1E', readBatteryLevel, handleError);
+});
+
 button.on('select', () => {
-  global.ble.scan([], 10, function(device) {
+  global.ble.scan([], 5, function (device) {
     console.log(JSON.stringify(device));
+  }, function (error) {
+    console.log(JSON.stringify(error));
   });
+  textView.text = 'Scan!';
 });
 
 
 
+
+function readBatteryLevel(data) {
+  console.log('Connected');
+  console.log(JSON.stringify(data));
+  console.log('.............................');
+  console.log('.............................');
+  global.ble.read('18:7A:93:12:0E:1E', deviceName.service, deviceName.level, onReadBatteryLevel, handleError);
+}
+
+function onReadBatteryLevel(data) {
+  console.log('On Read Data');
+  console.log(data);
+  console.log(JSON.stringify(data));
+  textView.text = JSON.stringify(data);
+}
+
+
 function startScanSuccess(result) {
-  consle.log(result);
+  console.log(result);
 }
 
 function handleError(error) {
+  console.log(JSON.stringify(error));
   textView.text = error;
+}
+
+function handleError2() {
+  textView.text = 'Fehler!';
 }
 
 //function _showMessage() {
